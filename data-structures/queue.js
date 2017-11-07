41a0fe8ff5a9a535e22dfde7e3f81eff63612ace
Michelle Huynh <michhh.12@gmail.com>
2017-11-07 15:30:13 -0800

    constructors for node and queue in queues

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -1,11 +1,13 @@
-function Node() {
-  // good luck. add any arguments you deem necessary.
-}
-
 // - Write a constructor function for a Node
 //     - it should have a value property which is assigned to a value passed as a parameter to the 
 // constructor function
 //     - it should have a next property initialized to null
+
+function Node(val) {
+  this.value = val;
+  this.next = null;
+}
+
 
 // - Write a constructor function for a Queue
 //     - it should have a size property initialized to 0   
@@ -13,27 +15,23 @@ function Node() {
 //     - it should have a last property initialized to null
 
 function Queue() {
-  // good luck. add any arguments you deem necessary.
+  this.first = null;
+  this.last = null;
+  this.size = 0;
 }
 
 // enqueue
-
 // This function adds the value to the end of the queue. This should be an O(1) operation and return the new size of the queue.
-
 Queue.prototype.enqueue = function() {
-  // good luck. add any arguments you deem necessary.
+  
 }
 
 // dequeue
-
 // This function removes the value at the beginning of the queue. This should be an O(1) operation and return the value removed. Note: The tests for dequeue rely on enqueue working
-
 Queue.prototype.dequeue = function() {
-  // good luck. add any arguments you deem necessary.
 }
 
 // peek - This function returns the first value in the queue.
-
 Queue.prototype.peek = function() {
-  // good luck. add any arguments you deem necessary.
+  
 }
