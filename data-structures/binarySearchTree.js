b11549abb7276205fa1db3437571c231e5100675
Michelle Huynh <michhh.12@gmail.com>
2017-11-12 12:46:35 -0800

    toArray for BST

--- data-structures/binarySearchTree.js
+++ data-structures/binarySearchTree.js
@@ -81,13 +81,37 @@ BinarySearchTree.prototype.findIteratively = function(val) {
 }
 
 // This function should find a node in a binary tree. It should return the node if found, otherwise return undefined.
-BinarySearchTree.prototype.findRecursively = function() {
-
+BinarySearchTree.prototype.findRecursively = function(val, currentNode=this.root) {
+  if (!currentNode) {
+    return undefined;
+  }
+  else if (val < currentNode.value) {
+    return this.findRecursively(val, currentNode.left);
+  }
+  else if (val > currentNode.value) {
+    return this.findRecursively(val, currentNode.right);
+  }
+  else {
+    return currentNode;
+  }
 }
 
 // This function should convert a binary search tree into an array of nodes from smallest to largest.
-BinarySearchTree.prototype.toArray = function() {
-
+BinarySearchTree.prototype.toArray = function(currentNode=this.root) {
+  var result = [];
+  if (!currentNode) {
+    return result;
+  }
+  if (currentNode.left) {
+    result = result.concat(this.toArray(currentNode.left));
+  }
+  
+  result.push(currentNode.value);
+  
+  if (currentNode.right) {
+    result = result.concat(this.toArray(currentNode.right));
+  }
+  return result;
 }
 
 // This function should search through each node in the binary search tree using pre-order depth first search and return an array containing each node's value.
