391ac6bb02abf8d2e2b711a0eba45aa71d6182b5
Michelle Huynh <michhh.12@gmail.com>
2017-11-16 15:17:16 -0800

    Created isSubsequence.js

---
+++ miscellaneous/isSubsequence.js
@@ -0,0 +1,7 @@
+/**
+ * Implement a function called isSubsequence which takes in two strings and
+ * checks whether the characters in the first string form a subsequence of the
+ * characters in the second string. In other words, the function should check
+ * whether the characters in the first string appear somewhere on the second
+ * string, without their order changing.
+ */
