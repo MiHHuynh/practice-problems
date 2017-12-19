ddf3801d6a393a28c7f9e5c2c1cef0485bee7c60
Michelle Huynh <michhh.12@gmail.com>
2017-12-18 17:21:44 -0800

    highestProduct practice problem with brute force approach

--- miscellaneous/highestProductFromThreeIntegers.js
+++ miscellaneous/highestProductFromThreeIntegers.js
@@ -3,5 +3,18 @@
 // The input arrayOfInts will always have at least three integers.
 
 function highestProductFromThreeIntegers(arrayOfInts) {
-  
+  // first: brute force approach with O(n^3) solution
+  var currentMax = 0;
+  for (let i = 0; i < arrayOfInts.length; i++) {
+    for (let j = 0; j < arrayOfInts.length; j++) {
+      for (let k = 0; k < arrayOfInts.length; k++) {
+        if (i !== j && i !== k && j !== k) {
+          currentMax = Math.max(currentMax, arrayOfInts[i]*arrayOfInts[j]*arrayOfInts[k]);  
+        }
+      }
+    }
+  }
+  return currentMax;
 }
+
+highestProductFromThreeIntegers([1,2,3,4,5]);
