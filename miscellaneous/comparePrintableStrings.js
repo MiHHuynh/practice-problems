1a7c6913212a60d59300e1cf53546acc4c1ba261
Michelle Huynh <michhh.12@gmail.com>
2017-12-25 14:57:34 -0800

    comparePrintableStrings exercise with solution

--- miscellaneous/comparePrintableStrings.js
+++ miscellaneous/comparePrintableStrings.js
@@ -5,54 +5,43 @@ function comparePrintableStrings(arrayOfKeypresses1, arrayOfKeypresses2) {
   var ptr1 = arrayOfKeypresses1.length - 1;
   var ptr2 = arrayOfKeypresses2.length - 1;
   while (ptr1 >= 0 && ptr2 >= 0) {
-    [ptr1, ptr2] = findIndicesOfPairToCompare(arrayOfKeypresses1, arrayOfKeypresses2, ptr1, ptr2);
-    if (arrayOfKeypresses1[ptr1] !== arrayOfKeypresses2[ptr2]) {
+    let kp1CharObj = findPrintableCharacter(arrayOfKeypresses1, ptr1);
+    let kp2CharObj = findPrintableCharacter(arrayOfKeypresses2, ptr2);
+    if (kp1CharObj["character"] !== kp2CharObj["character"]) {
       return false;
     }
-    if (ptr1 === 0 && ptr2 === 0) {
-      return true;
-    }
     else {
-      findIndicesOfPairToCompare(arrayOfKeypresses1, arrayOfKeypresses2, --ptr1, --ptr2);
+      ptr1 = kp1CharObj["nextIndex"];
+      ptr2 = kp2CharObj["nextIndex"];
     }
   }
+  return true;
 }
 
-// handles backspaces
-function findIndicesOfPairToCompare(arrayOfKeypresses1, arrayOfKeypresses2, ptr1, ptr2) {
-  var areValidCharsNotYetFound = true;
-  var bsCounter1 = 0;
-  var bsCounter2 = 0;
-  while (areValidCharsNotYetFound) {
-    if (isBackspace(arrayOfKeypresses1[ptr1])) {
-      ptr1--;
-      bsCounter1++;
+// finds first printable character from starting index indicated; defaults to last character if none passed in
+// returns an object with values of next index to start checking at and first printable char found from previous
+// starting point
+function findPrintableCharacter(arrayOfKeypresses, ptr=arrayOfKeypresses.length-1) {
+  var bsCounter= 0;
+  while (ptr >= 0) {
+    if (isBackspace(arrayOfKeypresses[ptr])) {
+      ptr--;
+      bsCounter++;
     }
-    if (isBackspace(arrayOfKeypresses2[ptr2])) {
-      ptr2--;
-      bsCounter2++;
-    }
-    if (!isBackspace(arrayOfKeypresses1[ptr1]) && hadBackspaces(bsCounter1)) {
-      ptr1 -= bsCounter1;
-      bsCounter1 = 0;
-    }
-    if (!isBackspace(arrayOfKeypresses2[ptr2]) && hadBackspaces(bsCounter2)) {
-      ptr2 -= bsCounter2;
-      bsCounter2 = 0;
-    }
-    if (!isBackspace(arrayOfKeypresses1[ptr1]) && !isBackspace(arrayOfKeypresses2[ptr2])) {
-      break;
-    }
+    if (!isBackspace(arrayOfKeypresses[ptr])) {
+      if (bsCounter > 0) {
+        ptr -= bsCounter;
+        bsCounter = 0;
+      }
+      else if (bsCounter === 0) return { "nextIndex": --ptr, "character": arrayOfKeypresses[ptr] };
+    }    
   }
-  return [ptr1, ptr2];
+  // if array is all backspaces, or there are more backspaces than characters
+  return { "nextIndex": --ptr, "character" : "" };
 }
 
 function isBackspace(str) {
   return str === "BS";
-}
-
-function hadBackspaces(n) {
-  return n > 0;
 }
 
 var kp1 = ['a', 'b', 'c', 'BS', 'c', 'BS', 'x'];
@@ -60,5 +49,14 @@ var kp2 = ['a', 'b', 'c', 'x', 'r', 'r', 'BS', 'BS'];
 var kp3 = ['a', 'b', 'c', 'x'];
 var kp4 = ['BS'];
 var kp5 = ['x'];
+var kp6 = ['BS', 'BS', 'BS'];
+var kp7 = ['a', 'b', 'BS', 'BS', 'BS', 'BS'];
 
-comparePrintableStrings(kp3, kp2);
+comparePrintableStrings(kp3, kp3); // true
+comparePrintableStrings(kp3, kp2); // true
+comparePrintableStrings(kp3, kp4); // false
+comparePrintableStrings(kp4, kp4); // true
+comparePrintableStrings(kp4, kp5); // false
+comparePrintableStrings(kp5, kp5); // true
+comparePrintableStrings(kp6, kp4); // true
+comparePrintableStrings(kp6, kp7); // true
