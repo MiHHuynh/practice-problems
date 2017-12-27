f36a01c2b0e940ac3fc98aea60254c769b9fe2ea
Michelle Huynh <michhh.12@gmail.com>
2017-12-26 19:41:39 -0800

    multiplyAllExceptCurrentIndex solution

--- miscellaneous/multiplyAllExceptCurrentIndex.js
+++ miscellaneous/multiplyAllExceptCurrentIndex.js
@@ -21,15 +21,34 @@ function multiplyAllExceptCurrentIdx(arr) {
 function multiplyAllExceptCurrentIdx(arr) {
   var allProductsBefore = [];
   var allProductsAfter = [];
+  var currentProduct = 1;
+  // construct the array of values before
   for (let i = 0; i < arr.length; i++) {
-    if (i === 0) {
-      allProductsBefore.push(1);
+    if (i !== 0) {
+      currentProduct = currentProduct * arr[i-1];
     }
-    else {
-      
-    }
+    allProductsBefore.push(currentProduct);
   }
+
+  // construct the array of values after
+  currentProduct = 1;
+  for (let j = arr.length-1; j >= 0; j--) {
+    if (j !== arr.length-1) {
+      currentProduct = currentProduct * arr[j+1];
+    }
+    allProductsAfter[j] = currentProduct; // avoiding unshift to keep time to O(n)
+  }
+
+  var result = [];
+  for (let k = 0; k < arr.length; k++) {
+    let product = allProductsBefore[k] * allProductsAfter[k];
+    result.push(product);
+  }
+  return result;
 }
 
 
 multiplyAllExceptCurrentIdx([1,7,32,5]);
+
+// Before: [1, 1, 7, 224]
+// After: [1120, 160, 5, 1]
